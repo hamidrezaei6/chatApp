33b479440e4b118a501192860a0b631713f39819
hamid <hamid.rezaei6@gmail.com>
2024-12-16 20:46:36 +0330

    make the messages seen when the other person is inside the chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -8,53 +8,57 @@ import datetime
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
-
         try:
             user_channel = models.UserChannel.objects.get(user=self.scope.get('user'))
             user_channel.channel_name = self.channel_name
             user_channel.save()
-
         except:
             user_channel = models.UserChannel()
             user_channel.user = self.scope.get('user')
             user_channel.channel_name = self.channel_name
             user_channel.save()
-
-
-
         self.person_id = self.scope.get('url_route').get('kwargs').get('id')
 
     def receive(self, text_data):
         text_data = json.loads(text_data)
-
         other_user = User.objects.get(id=self.person_id)
 
-        now = datetime.datetime.now()
-        date = now.date()
-        time = now.time()
+        if text_data.get('type') == 'new_message':
+            now = datetime.datetime.now()
+            date = now.date()
+            time = now.time()
 
+            new_message = models.Message()
+            new_message.from_who = self.scope.get('user')
+            new_message.to_who = User.objects.get(id=self.person_id)
+            new_message.message = text_data.get('message')
+            new_message.date = date
+            new_message.time = time
+            new_message.has_been_seen = False
+            new_message.save()
 
-        new_message = models.Message()
-        new_message.from_who = self.scope.get('user')
-        new_message.to_who = User.objects.get(id=self.person_id)
-        new_message.message = text_data.get('message')
-        new_message.date = date
-        new_message.time = time
-        new_message.has_been_seen = False
-        new_message.save()
+            try:
+                user_channel_name = models.UserChannel.objects.get(user=other_user)
+                data = {
+                    'type': 'receiver_function',
+                    'type_of_data': 'new_message',
+                    'data': text_data.get('message')
+                }
 
-        try:
-            user_channel_name = models.UserChannel.objects.get(user=other_user)
-            data = {
-                'type': 'receiver_function',
-                'type_of_data': 'new_message',
-                'data': text_data.get('message')
-            }
+                async_to_sync(self.channel_layer.send)(user_channel_name.channel_name, data)
+            except:
+                pass
+        elif text_data.get('type') == "i_have_seen_the_messages":
+            try:
+                user_channel_name = models.UserChannel.objects.get(user=other_user)
+                data = {
+                    'type': 'receiver_function',
+                    'type_of_data': "the_messages_has_been_seen_from_the_other"
+                }
+                async_to_sync(self.channel_layer.send)(user_channel_name.channel_name, data)
+            except:
+                pass
 
-            async_to_sync(self.channel_layer.send)(user_channel_name.channel_name, data)
-        except:
-            pass
-
-    def receiver_function(self, the_data_that_will_come_from_layer):
-        data = json.dumps(the_data_that_will_come_from_layer)
+    def receiver_function(self, the_data_that_will_come_from_the_layer):
+        data = json.dumps(the_data_that_will_come_from_the_layer)
         self.send(data)
